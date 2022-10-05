55fceda557a988aa3068eb19f862f0675e63f251
codingprana <114824861+codingprana@users.noreply.github.com>
2022-10-04 21:37:22 -0400

    rebuild

--- List_ADT/List.h
+++ List_ADT/List.h
@@ -61,3 +61,47 @@ void List::Insert( int index, int value)
   delete [] OldArray;
   
 }
+
+int List::Search(int value) {
+  // Looping through the array elements
+  // return the array index if value is found
+  for (int i = 0; i < m_count; ++i) {
+    if (m_items[i] == value) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void List::Remove(int index) {
+  // Check if the index is out of bound
+  if (index < 0 || index > m_count)
+    return;
+  
+  // Copy the current array
+  int *oldArray = m_items;
+
+  // Decrease the array length
+  --m_count;
+
+  // Initialize a new array
+  m_items = new int[m_count];
+
+  // Fill the new array and
+  // remove the selected index
+  for (int i = 0, j = 0; i < m_count; ++i, ++j) {
+    if (i == index) {
+      ++j;
+    }
+    else {
+      m_items[i] = oldArray[j];
+    }
+  }
+
+  // Remove the copied array
+  delete []oldArray;
+}
+
+int List::Count() {
+  return m_count;
+}
---
+++ List_ADT/main.cpp
@@ -0,0 +1,52 @@
+// Project: List.cbp
+// File: main.cpp
+#include "List.h"
+
+using namespace std;
+
+int main() {
+  // Initialize a list
+  List list = List();
+
+  // Add several items to the list
+  list.Insert(0, 1);
+  //list.Insert(1, 1);
+  list.Insert(1, 2);
+  list.Insert(2, 3);
+  //list.Insert(4, 5);
+  //list.Insert(5, 8);
+  list.Insert(3, 13);
+
+  // Print current list
+  cout << "List element:" << endl;
+  for (int i = 0; i < list.Count(); ++i) {
+    cout << list.Get(i) << " ";
+  }
+  cout << "\n\n";
+
+  // Insert several items in the middle of the List
+  list.Insert(1, 2);
+  list.Insert(4, 5);
+  list.Insert(5, 8);
+
+  // Print current list
+  cout << "List element:" << endl;
+  for (int i = 0; i < list.Count(); ++i) {
+    cout << list.Get(i) << " ";
+  }
+  cout << "\n\n";
+
+  // Search value 71
+  int test_1 = 71;
+  cout << "Search element " << test_1 << endl;
+  int result = list.Search(test_1);
+  if (result == -1) {
+    cout << test_1 << " is not found\n";
+  }
+  else {
+    cout << test_1 << " is found at index " << result;
+  }
+  cout << "\n\n";
+
+  return 0;
+}
